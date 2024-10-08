af4e4ffe92eaa0b30323530ab6c45f14852c35c0
Dawson-sk <dawson.skelley@gmail.com>
2024-10-07 21:58:21 -0400

    A1: Forgot to be commiting my code, 1 Step away from completeing

---
+++ circleshape.py
@@ -0,0 +1,25 @@
+import pygame
+
+class CircleShape(pygame.sprite.Sprite):
+	def __init__(self, x, y, radius):
+		if hasattr(self, "containers"):
+			super().__init__(self.containers)
+		else:
+			super().__init__()
+
+		self.position = pygame.Vector2(x, y)
+		self.velocity = pygame.Vector2(0, 0)
+		self.radius = radius
+
+	def draw(self, screen):
+		pass
+
+	def update(self, dt):
+		pass
+
+	def collision(self, obj) -> bool:
+		dist = pygame.math.Vector2.distance_to(self.position, obj.position)
+		if dist < (self.radius + obj.radius):
+			return True
+		else:
+			return False
--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import pygame
+import sys
+
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
+
 
 def main():
 	print("Starting asteroids!")
@@ -7,14 +14,56 @@ def main():
 	print(f"Screen height: {SCREEN_HEIGHT}")
 
 	pygame.init()
+	clock = pygame.time.Clock()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	asteroids = pygame.sprite.Group()
+	shots = pygame.sprite.Group()
+
+
+	Player.containers = (updatable, drawable)
+	Asteroid.containers = (asteroids, updatable, drawable)
+	AsteroidField.containers = (updatable)
+	Shot.containers = (shots, updatable, drawable)
+
+
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+	field = AsteroidField()
+
+	dt = 0
 
 	while True:
 		for event in pygame.event.get():
-			if event == pygame.QUIT:
+			if event.type == pygame.QUIT:
 				return
+
+
+		for u in updatable:
+			u.update(dt)
+
 		screen.fill((0,0,0))
+
+
+		for d in drawable:
+			d.draw(screen)
+
+		for a in asteroids:
+			if a.collision(player) == True:
+				print("Game over!")
+				sys.exit()
+			for s in shots:
+				if s.collision(a):
+					s.kill()
+					a.kill()
+
 		pygame.display.flip()
+
+
+		dt = clock.tick(60) / 1000
+
+
 
 if __name__ == "__main__":
 	main()
